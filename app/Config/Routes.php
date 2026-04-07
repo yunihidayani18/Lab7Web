bb3600066da0417f59323cc96bddf97e45d2acf3
yunihidayani18 <57063216+yunihidayani18@users.noreply.github.com>
2026-04-08 03:28:48 +0700

    Add files via upload

---
+++ app/Config/Routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/about', 'Page::about');
+$routes->get('/contact', 'Page::contact');
+$routes->get('/faqs', 'Page::faqs');
+$routes->get('/page/tos', 'Page::tos');
+
+
